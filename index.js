02573c5f3bfa997e632aadc6bb8392ec05c04064
Doddy MATABARO <matabarododdy@gmail.com>
2022-10-19 14:55:11 +0200

    use middlware

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next)=>{
+    console.log("in the middlware!");
+    next(); // allows the request to continue  to  the next middlware
+})
+app.use((req, res, next)=>{
+    console.log("an another middlware!");
+    res.send('<h1>Hello from express !</h1>')
+})
 const server = http.createServer(app);
 
 server.listen(3000);
